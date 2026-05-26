953f97cd18ff9827a4f2650f359ad0c060ac3cea
avioncitobg <benjaminbg_1995@hotmail.com>
2026-05-26 11:41:27 -0500

    Se agrega view para el listado de productos

---
+++ views/listar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MVC</title>
+</head>
+
+<body>
+    <h1>Listado de Productos</h1>
+    <table border="1">
+        <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+        </tr>
+        <?php foreach ($productos as $producto): ?>
+            <tr>
+                <td><?php echo $producto['id']; ?></td>
+                <td><?php echo $producto['nombre']; ?></td>
+                <td><?php echo $producto['precio']; ?></td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</body>
+
+</html>
